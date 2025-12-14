2de92f561bb439e58d8db3728f2150a4e9c315be
ali <ali.pyprogrammer@gmail.com>
2025-12-14 19:56:45 +0330

    update setting

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +9,6 @@ urlpatterns = [
     path('api/ui/', include('ui.urls')),
     path('api/product/', include('Product.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
