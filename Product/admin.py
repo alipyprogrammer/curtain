b81b0565582f2dcbd9be8975982eeea094f0daaf
ali <ali.pyprogrammer@gmail.com>
2025-12-14 19:26:46 +0330

    admin Product

--- Product/admin.py
+++ Product/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+admin.site.register(Brand)
+admin.site.register(Tag)
+admin.site.register(MainCategories)
+admin.site.register(Subcategories)
+admin.site.register(Properties)
+admin.site.register(Gallery)
+admin.site.register(Attributes)
+admin.site.register(EventType)
+admin.site.register(Packaging)
+admin.site.register(Product)
+admin.site.register(Reviews)
