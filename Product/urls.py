782ceef313a121adedf3a25e92e5eb59bfb8e30d
alipy <ali.pyprogrammer@gmail.com>
2025-12-26 15:45:55 +0330

    update urls product

--- Product/urls.py
+++ Product/urls.py
@@ -1,5 +1,24 @@
 from django.urls import path, include, re_path
 from . import views
 
+
+
 urlpatterns = [
+    #Detail
+    re_path(
+            r'product/(?P<main_category>[-\w]+)/(?P<sub_category>[-\w]+)/(?P<slug>[-\w]+)/',
+            views.product_detail, name="ProductDetail"
+    ),
+
+    re_path(
+            r'product/(?P<main_category>[-\w]+)/(?P<sub_category>[-\w]+)/',
+            views.product_category_list, name="ProductDetail"
+    ),
+
+    re_path(
+            r'product/(?P<main_category>[-\w]+)/',
+            views.product_category_list, name="ProductDetail"
+    )
+
 ]
+    
